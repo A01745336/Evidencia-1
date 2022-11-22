961507fe4d1917fab5455114e5f7b212d1cfa15d
Nadia <nadiapaola145@gmail.com>
2022-11-21 23:40:14 -0600

    Cambios en el codigo

---
+++ Evidencia1_Unity/Assets/Scripts/ServerPruebas/AgentController.cs
@@ -0,0 +1,271 @@
+﻿// TC2008B. Sistemas Multiagentes y Gráficas Computacionales
+// C# client to interact with Python. Based on the code provided by Sergio Ruiz.
+// Octavio Navarro. October 2021
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[Serializable]
+public class WalleData
+{
+    public string id;
+    public float x, y, z;
+
+    public WalleData(string id, float x, float y, float z)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[Serializable]
+
+public class AgentsData
+{
+    public List<WalleData> positions;
+
+    public AgentsData() => this.positions = new List<WalleData>();
+}
+
+[Serializable]
+public class CajasData
+{
+    public string id;
+    public float x, y, z;
+
+    public CajasData(string id, float x, float y, float z)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[Serializable]
+
+public class CajaData
+{
+    public List<CajasData> positions;
+
+    public CajaData() => this.positions = new List<CajasData>();
+}
+
+[Serializable]
+public class RepisasData
+{
+    public string id;
+    public float x, y, z;
+
+    public RepisasData(string id, float x, float y, float z)
+    {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[Serializable]
+
+public class RepisaData
+{
+    public List<RepisasData> positions;
+
+    public RepisaData() => this.positions = new List<RepisasData>();
+}
+
+
+public class AgentController : MonoBehaviour
+{
+    string serverUrl = "http://localhost:8521";
+    string getWalleEndpoint = "/getWalle";
+    string getCajasEndpoint = "/getCajas";
+    string getRepisasEndpoint = "/getRepisas";
+    string sendConfigEndpoint = "/init";
+    string updateEndpoint = "/update";
+    AgentsData wallesData;
+    CajaData cajasData;
+    RepisaData repisasData;
+    Dictionary<string, GameObject> walles;
+    Dictionary<string, GameObject> cajas;
+    Dictionary<string, GameObject> repisas;
+    Dictionary<string, Vector3> prevPositions, currPositions;
+
+    bool updated = false, started = false;
+
+    public GameObject wallePrefab, cajaPrefab, repisaPrefab, floor;
+    public int NAgents, width, height;
+    public float timeToUpdate = 5.0f;
+    private float timer, dt;
+
+    void Start()
+    {
+        wallesData = new AgentsData();
+        cajasData = new CajaData();
+        repisasData = new RepisaData();
+
+        prevPositions = new Dictionary<string, Vector3>();
+        currPositions = new Dictionary<string, Vector3>();
+
+        walles = new Dictionary<string, GameObject>();
+        cajas = new Dictionary<string, GameObject>();
+        repisas = new Dictionary<string, GameObject>();
+
+        floor.transform.localScale = new Vector3((float)(width + 1) / 10, 1, (float)(height + 1) / 10);
+        floor.transform.localPosition = new Vector3((float)width/2-0.5f, 0, (float)height/2-0.5f);
+
+        timer = timeToUpdate;
+
+        StartCoroutine(SendConfiguration());
+    }
+
+    private void Update()
+    {
+        if(timer < 0)
+        {
+            timer = timeToUpdate;
+            updated = false;
+            StartCoroutine(UpdateSimulation());
+        }
+
+        if (updated)
+        {
+            timer -= Time.deltaTime;
+            dt = 1.0f - (timer / timeToUpdate);
+
+            foreach(var agent in currPositions)
+            {
+                Vector3 currentPosition = agent.Value;
+                Vector3 previousPosition = prevPositions[agent.Key];
+
+                Vector3 interpolated = Vector3.Lerp(previousPosition, currentPosition, dt);
+                Vector3 direction = currentPosition - interpolated;
+
+                walles[agent.Key].transform.localPosition = interpolated;
+                if(direction != Vector3.zero) walles[agent.Key].transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
+    }
+
+    IEnumerator UpdateSimulation()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + updateEndpoint);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            StartCoroutine(GetWallesData());
+            StartCoroutine(GetCajasData());
+            StartCoroutine(GetRepisasData());
+        }
+    }
+
+    IEnumerator SendConfiguration()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("NAgents", NAgents.ToString());
+        form.AddField("width", width.ToString());
+        form.AddField("height", height.ToString());
+
+        UnityWebRequest www = UnityWebRequest.Post(serverUrl + sendConfigEndpoint, form);
+        www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Configuration upload complete!");
+            Debug.Log("Getting Agents positions");
+            StartCoroutine(GetWallesData());
+            StartCoroutine(GetCajasData());
+            StartCoroutine(GetRepisasData());
+        }
+    }
+
+    IEnumerator GetWallesData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getWalleEndpoint);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            wallesData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+
+            foreach(WalleData agent in wallesData.positions)
+            {
+                Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
+
+                    if(!started)
+                    {
+                        prevPositions[agent.id] = newAgentPosition;
+                        walles[agent.id] = Instantiate(wallePrefab, newAgentPosition, Quaternion.identity);
+                    }
+                    else
+                    {
+                        Vector3 currentPosition = new Vector3();
+                        if(currPositions.TryGetValue(agent.id, out currentPosition))
+                            prevPositions[agent.id] = currentPosition;
+                        currPositions[agent.id] = newAgentPosition;
+                    }
+            }
+
+            updated = true;
+            if(!started) started = true;
+        }
+    }
+
+    IEnumerator GetCajasData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getCajasEndpoint);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            cajasData = JsonUtility.FromJson<CajaData>(www.downloadHandler.text);
+
+            Debug.Log(cajasData.positions);
+
+            foreach(CajasData obstacle in cajasData.positions)
+            {
+                Instantiate(cajaPrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+            }
+        }
+    }
+    IEnumerator GetRepisasData()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getRepisasEndpoint);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            repisasData = JsonUtility.FromJson<RepisaData>(www.downloadHandler.text);
+
+            Debug.Log(repisasData.positions);
+
+            foreach(RepisasData obstacle in repisasData.positions)
+            {
+                Instantiate(repisaPrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ serverPruebas/server.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+from AcomodarCajasModel import *
+
+# Size of the board:
+number_agents = 5
+width = 28
+height = 28
+almacenModel = None
+currentStep = 0
+
+app = Flask("Acomodar Cajas Model")
+
+
+@app.route('/init', methods=['POST', 'GET'])
+def initModel():
+    global currentStep, almacenModel, number_agents, width, height
+
+    if request.method == 'POST':
+        number_agents = int(request.form.get('NAgents'))
+        width = int(request.form.get('width'))
+        height = int(request.form.get('height'))
+        currentStep = 0
+
+        print(request.form)
+        print(number_agents, width, height)
+        almacenModel = AlmacenModel(number_agents, width, height)
+
+        return jsonify({"message": "Parameters recieved, model initiated."})
+
+
+@app.route('/getWalle', methods=['GET'])
+def getWalle():
+    global AlmacenModel
+
+    if request.method == 'GET':
+        agentPositions = [{"id": str(a.unique_id), "x": x, "y": 1, "z": z}
+                          for (a, x, z) in AlmacenModel.grid.coord_iter()
+                          if isinstance(a, Walle)]
+
+        return jsonify({'positions': agentPositions})
+
+
+@app.route('/getCajas', methods=['GET'])
+def getObstacles():
+    global AlmacenModel
+
+    if request.method == 'GET':
+        cajaPositions = [{"id": str(a.unique_id), "x": x, "y": 1, "z": z}
+                         for (a, x, z) in AlmacenModel.grid.coord_iter()
+                         if isinstance(a, Cajas)]
+
+        return jsonify({'positions': cajaPositions})
+
+
+@app.route('/getRepisas', methods=['GET'])
+def getRepisas():
+    global AlmacenModel
+
+    if request.method == 'GET':
+        repisaPositions = [{"id": str(a.unique_id), "x": x, "y": 1, "z": z}
+                           for (a, x, z) in AlmacenModel.grid.coord_iter()
+                           if isinstance(a, Cajas)]
+
+        return jsonify({'positions': repisaPositions})
+
+
+@app.route('/update', methods=['GET'])
+def updateModel():
+    global currentStep, almacenModel
+    if request.method == 'GET':
+        almacenModel.step()
+        currentStep += 1
+        return jsonify({'message': f'Model updated to step {currentStep}.',
+                        'currentStep': currentStep})
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8521, debug=True)
